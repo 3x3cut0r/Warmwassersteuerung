e348a09b7628bff43e9e80586b64cc9bdac84a6d
3x3cut0r <julianreith@gmx.de>
2025-01-05 18:07:52 +0100

    fix path; added some outputs

--- scripts/copy_pico-win.cmd
+++ scripts/copy_pico-win.cmd
@@ -7,8 +7,6 @@ if "%1"=="" (
     set PORT=%1
 )
 
-cd ..
-
 ampy --port %PORT% ls 2>NUL
 
 echo deleting files...
@@ -30,6 +28,7 @@ ampy --port %PORT% put main.py main.py 2>NUL
 ampy --port %PORT% put config.json config.json 2>NUL
 ampy --port %PORT% put webserver.py webserver.py 2>NUL
 
+echo "  mkdir utils..."
 ampy --port %PORT% mkdir utils 2>NUL
 ampy --port %PORT% put utils/get_bool.py utils/get_bool.py 2>NUL
 ampy --port %PORT% put utils/get_float.py utils/get_float.py 2>NUL
@@ -37,6 +36,7 @@ ampy --port %PORT% put utils/get_int.py utils/get_int.py 2>NUL
 ampy --port %PORT% put utils/log.py utils/log.py 2>NUL
 ampy --port %PORT% put utils/log_level.py utils/log_level.py 2>NUL
 
+echo "  mkdir src..."
 ampy --port %PORT% mkdir src 2>NUL
 ampy --port %PORT% put src/button.py src/button.py 2>NUL
 ampy --port %PORT% put src/config.py src/config.py 2>NUL
@@ -50,6 +50,7 @@ ampy --port %PORT% put src/rlock.py src/rlock.py 2>NUL
 ampy --port %PORT% put src/temp.py src/temp.py 2>NUL
 ampy --port %PORT% put src/wifi.py src/wifi.py 2>NUL
 
+echo "  mkdir web..."
 ampy --port %PORT% mkdir web 2>NUL
 ampy --port %PORT% put web/index.html web/index.html 2>NUL
 ampy --port %PORT% put web/styles.css web/styles.css 2>NUL
